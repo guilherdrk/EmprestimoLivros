897ac5d738f94f65cfc62f7c7c792b546654e81e
guilherdrk <guibem20@gmail.com>
2024-05-06 14:28:28 -0300

    Feat: Criação de mensagens de alerta para cadastro, edição e exclusão

--- EmprestimoLivros/wwwroot/js/site.js
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -33,4 +33,12 @@ $(document).ready(function () {
         }
     });
 
+    setTimeout(function ()
+    {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+
+        })
+    }, 5000)
+
 });
